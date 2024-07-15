366e8f582b14aa8151c45b9b23c67ae471a2ab21
Manoj12012004 <117918138+Manoj12012004@users.noreply.github.com>
2024-07-15 18:40:28 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,8 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
+  credentials: true 
 }));
 app.use(express.json())
 dbConnect();
@@ -42,6 +43,19 @@ app.use((req, res, next) => {
     );
     next();
 });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.options('*', cors());
 
 
