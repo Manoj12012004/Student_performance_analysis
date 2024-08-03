bb8702eb7f92acf39dfc20b063cbf59ba894e367
Manoj12012004 <sandupatlamanojkumar@gmail.com>
2024-08-03 08:54:22 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import auth from './auth.js';
 import dotenv from "dotenv";
 import { spawn } from 'child_process';
 import path from "path";
+import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
